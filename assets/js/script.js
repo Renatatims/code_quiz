115b3befffa66700115b8fad75949e24b940b1c0
ReTims <renatatims@gmail.com>
2022-08-19 15:42:13 -0500

    Obeject variable created for questions + event Listener for alternatives

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-var questions = document.querySelector(".questions");
+var questionsEl = document.querySelector(".questions");
 var startButton = document.getElementById("startQuiz");
-var timeX = document.querySelector (".time")
+var timeX = document.getElementById ("time")
 var secondsLeft = 75;
 var alternatives = document.querySelector(".alternatives");
 
@@ -10,16 +10,58 @@ var alternative3 = document.getElementById("alternative3");
 var alternative4 = document.getElementById("alternative4");
 
 
-var question1 = "Which of the following are JavaScript Data Types?";
-var question2 =  "What are all the types of Pop up boxes available in JavaScript?"
-var question3 = " What is '====' operator? "
-var question4 = "Arrays in JavaScript can be used to store___"
-var question5 = "Which function is used to serialize an object into a JSON string in Javascript?"
+var questions = [
+    {question: "Which of the following are JavaScript Data Types?",
+    alternatives: ["a", "b", "c", "d"],
+    correctAnswer: "a",
+    },
+    {question: "What are all the types of Pop up boxes available in JavaScript?",
+    aleternatives: ["a", "b", "c", "d"],
+    correctAnswer: "b",
+    },
+    {question: " What is '====' operator? ",
+    aleternatives: ["a", "b", "c", "d"],
+    correctAnswer: "c",
+    },
+    {question: "Arrays in JavaScript can be used to store___",
+    aleternatives: ["a", "b", "c", "d"],
+    correctAnswer: "d",
+    },
+    {question: "Which function is used to serialize an object into a JSON string in Javascript?",
+    aleternatives: ["a", "b", "c", "d"],
+    correctAnswer: "e",
+    },
+];
 
+
+var qCounter = 0;
+
+function displayQuestion (){
+    questionsEl.textContent = questions[qCounter].question;
+    alternative1.textContent = "1. A";
+    alternative2.textContent = "2. B";
+   alternative3.textContent = "3. C";
+   alternative4.textContent = "4. D";
+
+}
+
+alternatives.addEventListener("click", function(event){
+    console.log(event);
+    console.log(event.target.textContent)
+  //*check if user clicked the correct answer*/
+  /*if the user clicks correct answer - moves to the next question */ 
+/*if wrong - than worng displayed - timer -10 seconds - move to the next question */
+
+qCounter ++;
+displayQuestion ();
+
+
+ })
 
 
 
 startButton.addEventListener ("click", function () {
+   
     var timer = setInterval (function() {
         secondsLeft--;
         timeX.textContent = secondsLeft + " seconds left";
@@ -29,22 +71,12 @@ startButton.addEventListener ("click", function () {
            /* sendMessage();*/
         }
     }, 1000);
-
-        
-   questions.textContent = question1;
-
-   alternative1.addEventListener("click", function(){
-   alternative1.textContent = "1. A";
- })
-
-   alternative1.textContent = "1. A";
-   alternative2.textContent = "2. B";
-   alternative3.textContent = "3. C";
-   alternative4.textContent = "4. D";
-
-
-   startButton.textContent = "SUBMIT";
     
+    /*startButton.setVisible(false);*/
+
+     displayQuestion();   
+     
+   
 })
 
 
