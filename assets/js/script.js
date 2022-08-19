223ec024aa4cac1f3bef9d51bcdb0037b22b9d0d
ReTims <renatatims@gmail.com>
2022-08-19 11:41:38 -0500

    Files added - index.html structure

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var questions = document.querySelector(".questions");
+var startQUiz = document.getElementById("#startQuiz");
+
+var question1 = "What's your name?"; 
+var question2 = "What's your favorite color";
+var question3 =  "How old are you?";
+
+
+
+
+function question1(){
+ questions.textContent = question1;
+
+ question1()
+
+}
+
