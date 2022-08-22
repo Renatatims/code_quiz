4e699e9e7a0d6162eb25e784a44c3b5af11ed1c7
ReTims <renatatims@gmail.com>
2022-08-21 23:30:52 -0500

    Display message rather the user selected correct or incorrect answer + if conditions added when the user finishes the test before the time ends + total score added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,42 @@
 var questionsEl = document.querySelector(".questions");
 var startButton = document.getElementById("startQuiz");
 var timeX = document.getElementById ("time")
+var timeCount = document.querySelector (".timeCount")
 var secondsLeft = 75;
-var alternatives = document.querySelector(".alternatives");
+var alternativesEl = document.querySelector(".alternatives");
+
 
 var alternative1 = document.getElementById("alternative1");
 var alternative2 = document.getElementById("alternative2");
 var alternative3 = document.getElementById("alternative3");
 var alternative4 = document.getElementById("alternative4");
+var highscores = document.getElementById("highscores");
+
+var messageEl = document.getElementById("message");
 
 
+var main = document.querySelector(".quiz");
 
 var questions = [
     {question: "Which of the following are JavaScript Data Types?",
-    alternatives: ["number", "string", "boolean", "all of the above"],
-    correctAnswer: alternatives[3],
+    alternativesQuiz: ["number", "string", "boolean", "all of the above"],
+    correctAnswer: alternative4,
     },
     {question: "How to stop an interval timer in Javascript?",
-    alternatives: ["clearInterval", "clearTimer", "intervalOver", "None of the above"],
-    correctAnswer: alternatives[0],
+    alternativesQuiz: ["clearInterval", "clearTimer", "intervalOver", "None of the above"],
+    correctAnswer: alternative1,
     },
     {question: " Commonly used data types DO NOT include ",
-    alternatives: ["strings", "booleans", "alerts", "numbers"],
-    correctAnswer: alternatives[2],
+    alternativesQuiz: ["strings", "booleans", "alerts", "numbers"],
+    correctAnswer: alternative3,
     },
     {question: "Arrays in JavaScript can be used to store___",
-    alternatives: ["numbers and strings", "other arrays", "booleans", "All of the above"],
-    correctAnswer: alternatives[3],
+    alternativesQuiz: ["numbers and strings", "other arrays", "booleans", "All of the above"],
+    correctAnswer: alternative4,
     },
     {question: "Which function is used to serialize an object into a JSON string in Javascript?",
-    alternatives: ["stringify()", "parse()", "convert()", "None of the above"],
-    correctAnswer: alternatives[0],
+    alternativesQuiz: ["stringify()", "parse()", "convert()", "None of the above"],
+    correctAnswer: alternative1,
     },
 ];
 
@@ -38,33 +44,43 @@ var questions = [
 var qCounter = 0;
 
 function displayQuestion (){
-    
+    if (qCounter<5){
     questionsEl.textContent = questions[qCounter].question;
-    alternative1.textContent = "1. " + questions[qCounter].alternatives[0];
-    alternative2.textContent = "2. " + questions[qCounter].alternatives[1];
-    alternative3.textContent = "3. " + questions[qCounter].alternatives[2];
-    alternative4.textContent = "4. " + questions[qCounter].alternatives[3];
-
+    alternative1.textContent = "1. " + questions[qCounter].alternativesQuiz[0];
+    alternative2.textContent = "2. " + questions[qCounter].alternativesQuiz[1];
+    alternative3.textContent = "3. " + questions[qCounter].alternativesQuiz[2];
+    alternative4.textContent = "4. " + questions[qCounter].alternativesQuiz[3];
+    
+}else{
+    main.textContent = "Highscores";
+    
 }
+};
 
-var correct = 0;
-var wrong = 0;
 
-alternatives.addEventListener("click", function(event){
-    var userClick = event.target.textContent;
+var totalScore = 0;
+
+alternativesEl.addEventListener("click", function(event){
+    var userClick = event.target;
   
+    //*check if user clicked the correct answer*/
+    /*if the user clicks correct answer - moves to the next question */
     if (userClick === questions[qCounter].correctAnswer){
-        correct++;
+        totalScore=totalScore+20;
+        messageEl.textContent = "Correct!";
+
+
+    /*if wrong - than wrong displayed - timer -10 seconds - move to the next question */    
     } else{
-        wrong--;
         secondsLeft = secondsLeft-10;
+        messageEl.textContent = "Wrong";
     }
     console.log(event);
 
     console.log(event.target.textContent)
-  //*check if user clicked the correct answer*/
-  /*if the user clicks correct answer - moves to the next question */ 
-/*if wrong - than worng displayed - timer -10 seconds - move to the next question */
+ 
+   
+
 
 qCounter++;
 displayQuestion ();
@@ -73,7 +89,7 @@ displayQuestion ();
  })
 
 
-
+ 
 startButton.addEventListener ("click", function () {
    
     var timer = setInterval (function() {
@@ -83,6 +99,9 @@ startButton.addEventListener ("click", function () {
             clearInterval(timer);
             timeX.textContent = "Time ended!";
            /* sendMessage();*/
+        } else if (qCounter>4){
+            clearInterval(timer);
+            timeCount.textContent = "Quiz ended!";
         }
     }, 1000);
     
@@ -91,7 +110,29 @@ startButton.addEventListener ("click", function () {
      displayQuestion();   
      
    
-})
+});
+
+
+
+
+/* THINKING - last question
+
+function lastQuestion (){
+    if (qCounter===5){
+        questionsEl.textContent = "Highscore";
+    }
+
+    lastQuestion();
+
+}
+*/
+
+highscores.addEventListener ("click", function (){
+    main.textContent = "Total Score = " + totalScore;
+   
+});
+
+
 
 
 /*
@@ -108,4 +149,11 @@ function sendMessage (){
     
 }
 */
+
+
+/* Remove the list items from the UL list from the home page*/
+/* DONE! Insert "wrong and correct lines when user selects an answer" */ 
+/* End the quiz and provide an input for the USer to insert his name and show the SCORE*/
+
+
 
